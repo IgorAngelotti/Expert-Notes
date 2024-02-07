3beb8d5933eb2b7179d0ebf019245087fa98bf5a
Igor Angelotti <105242564+IgorAngelotti@users.noreply.github.com>
2024-02-07 14:47:21 -0300

    Cards e adicionar texto

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import Logo from './assets/Logo-nlw-expert.svg'
+import { NewNoteCard } from './components/Newnote-card'
 import { NoteCard } from './components/note-card'
 
 export function App() {
@@ -17,20 +18,20 @@ export function App() {
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
 
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-200" >
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-          </p>
-        </div>
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NewNoteCard />
+        <NoteCard note={{
+          data: new Date(),
+          content: "Lembrar de terminar este desenvolvimento para garantir que eu possa evoluir no react e tailwind"
+        }} />
+
+        <NoteCard note={{
+          data: new Date(),
+          content: "Lembrar de terminar este desenvolvimento para garantir que eu possa evoluir no react e tailwind"
+        }} />
+        <NoteCard note={{
+          data: new Date(),
+          content: "Lembrar de terminar este desenvolvimento para garantir que eu possa evoluir no react e tailwind"
+        }} />
 
       </div>
     </div>
